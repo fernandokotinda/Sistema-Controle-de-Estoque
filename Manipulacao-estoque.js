3a0eccffc9b03b990032989ecd5706f0db189c0d
Fernando Kotinda <124843760+fernandokotinda@users.noreply.github.com>
2023-11-12 00:41:18 -0300

    tabela estoque concluida

--- Manipulacao-estoque.js
+++ Manipulacao-estoque.js
@@ -1,69 +1,302 @@
+//Mudar cor no select
+let selectElement = document.getElementById("type");
+
+selectElement.addEventListener("change", function () {
+    let selectedOption = selectElement.options[selectElement.selectedIndex];
+
+    if (selectedOption.classList.contains("white")) {
+        selectElement.style.color = "white";
+    } else {
+        selectElement.style.color = "gray"; 
+    }
+});
 
 // Adicionar tabela
 let addProduct = document.getElementById("addProduct");
 let downloadButton = document.getElementById("downloadButton");
+let popUp = document.getElementById("popUp");
+let exitPopUp = document.getElementById("fecharPopUp");
+let message = document.querySelector(".popUpMessage");
+let ok = document.querySelector("#ok");
 
 
-    addProduct.addEventListener('click', (ev) => {
+addProduct.addEventListener('click', (ev) => {
 
-        let productNameInput = document.getElementById("productName").value;
-        let productQuantity = document.getElementById("productQuantity").value;
-        let productQuantityInput = document.getElementById("productQuantity");
+        let name = document.querySelector('#productName').value;
+        let nameInput = document.querySelector('#productName');
+        let productQuantity = document.querySelector('#productQuantity').value;
+        let productQuantityInput = document.querySelector('#productQuantity');
         let select = document.getElementById('type');
-        let selectType = document.getElementById('type').value;
+        let selectValue = document.querySelector('#type').value;
         let code = document.getElementById('cod').value;
         let codeInput = document.getElementById('cod');
-
-        if(productNameInput !== '' && code !== '' && productQuantity !== '' && !productQuantityInput.classList.contains('erro') && !codeInput.classList.contains('erro') && selectType !== 'Tipo') {
-
-            ev.preventDefault();
-    
-            let table = document.getElementById("estoque1");
-    
-            let tr = document.createElement('tr');
-            table.appendChild(tr);
-    
-            let item = document.createElement('td');
-            item.classList.add('item');
-            item.innerText = productNameInput;
-    
-            let quantity = document.createElement('td');
-            quantity.classList.add('quantity');
-            quantity.innerText = productQuantity;
-    
-            tr.append(item, quantity)
+        let regex = /^[1-9]\d*$/;
+        let regexCode = /^[0-9]\d*$/;
+        
+        //Verificação formulário
+        if(name !== '' && code !== '' && productQuantity !== '' && !productQuantityInput.classList.contains('erro') && !codeInput.classList.contains('erro')) {
             
-            document.getElementById('productName').value = '';
-            document.getElementById('productQuantity').value = '';
-            document.getElementById('type').value = 'Tipo';
-            select.style.color = 'gray'
-            document.getElementById('cod').value = '';
+            ev.preventDefault();
+            
+            function errorQuantity() {
+
+                message.textContent = "Digite apenas números inteiros maiores que 0.";
+                productQuantityInput.classList.add('erro');
+                popUp.style.display = "block";
+
+                exitPopUp.addEventListener("click", function () { exitQuantity() });
+                ok.addEventListener("click", function () { exitQuantity() });
+                
+                function exitQuantity() {
+                    
+                    popUp.style.display = "none";
+                    productQuantityInput.classList.remove('erro');
+                    document.getElementById('productQuantity').value = '';
+                    
+                    if (selectValue !== 'Tipo') { select.style.color = 'white'; document.querySelector('#type').value = selectValue };
+                    if (regexCode.test(code)) { document.getElementById('cod').value = code };
+                    if (!code.length < 6) { document.getElementById('cod').value = code };
+                    
+                    if (selectValue === 'Tipo') {
+                        errorType();
+                    } else if (code.length < 6) {
+                        errorCodeLength();
+                    } else if (!regexCode.test(code)) {
+                        errorCodeRegex();
+                    }
+
+                }
+
+            }
+
+            function errorType() {
+
+                message.textContent = "Por favor, Escolha qual o tipo do Produto.";
+                select.classList.add('erro');
+                popUp.style.display = "block";
+                
+
+                exitPopUp.addEventListener("click", function () { exitType() });
+                ok.addEventListener("click", function () { exitType() });
+
+                function exitType() {
+
+                    popUp.style.display = "none";
+                    select.classList.remove('erro');
+                    select.value = 'Tipo';
+                    select.style.color = 'gray'
+                    
+                    if (regex.test(productQuantity)) { document.querySelector('#productQuantity').value = productQuantity };
+                    if (regexCode.test(code)) { document.getElementById('cod').value = code }
+                    if (!(code.length < 6)) { document.getElementById('cod').value = code }
+                    
+                    if (code.length < 6) {
+                        errorCodeLength();
+                    } else if (!regexCode.test(code)) {
+                        errorCodeRegex();
+                    }
+                    
+                }
+            }
+
+
+            function errorCodeLength() {
+
+                message.textContent = "O código deve conter exatos 6 números.";
+                codeInput.classList.add('erro')
+                popUp.style.display = "block";
+                
+                exitPopUp.addEventListener("click", function () { exitCodeLength() });
+                ok.addEventListener("click", function () { exitCodeLength() });
+                
+                function exitCodeLength() {
+                    
+                    popUp.style.display = "none";
+                    codeInput.classList.remove('erro')
+                    document.getElementById('cod').value = '';
+                    
+                    if (selectValue !== 'Tipo') { select.style.color = 'white'; document.querySelector('#type').value = selectValue };
+                    if (regex.test(productQuantity)) { document.querySelector('#productQuantity').value = productQuantity };
+                    
+                    if (!regexCode.test(code)) { errorCodeRegex() };
+                    
+                }
+
+                
+            }
+
+            function errorCodeRegex() {
+                
+                message.textContent = "O código deve conter apenas números inteiros maiores que 0.";
+                codeInput.classList.add('erro')
+                popUp.style.display = "block";
+                
+                exitPopUp.addEventListener("click", function () { exitCodeRegex() });
+                ok.addEventListener("click", function () { exitCodeRegex() });
+                
+                function exitCodeRegex() {
+                    
+                    popUp.style.display = "none";
+                    codeInput.classList.remove('erro')
+                    document.getElementById('cod').value = '';
+
+                    if (selectValue !== 'Tipo') { select.style.color = 'white'; document.querySelector('#type').value = selectValue };
+                    if (regex.test(productQuantity)) { document.querySelector('#productQuantity').value = productQuantity };
+                    
+                }
+
+
+            }
+
+            if (!regex.test(productQuantity)) {
+
+                errorQuantity();
+
+            } else if (selectValue === 'Tipo') {
+                
+                errorType();
+
+            } else if (code.length < 6) {
+                
+                errorCodeLength();
+                
+            } else if (!regexCode.test(code)) {
+                
+                errorCodeRegex();
+                
+            }
+
+            //Adicionando tabela
+            if(regex.test(productQuantity) && selectValue !== 'Tipo' && !(code.length < 6) && regexCode.test(code)) {
+
+                
+                if (document.querySelectorAll('#estoque tbody tr').length === 0) {
+
+                    function createTable() {
+
+                        let theadStock = document.querySelector('#estoque thead');
+                        let trThead = document.createElement('tr');
+                        theadStock.append(trThead);
+
+                        let produto = document.createElement('th');
+                        produto.setAttribute('scope', 'col');
+                        produto.innerText = 'Produto'
+
+                        let quantidade = document.createElement('th');
+                        quantidade.setAttribute('scope', 'col');
+                        quantidade.innerText = 'Quantidade'
+
+                        let tipoProduto = document.createElement('th');
+                        tipoProduto.setAttribute('scope', 'col');
+                        tipoProduto.innerText = 'Tipo';
+                        
+                        let codigoProduto = document.createElement('th');
+                        codigoProduto.setAttribute('scope', 'col');
+                        codigoProduto.innerText = 'Código';
+                        
+                        trThead.append(produto, quantidade, tipoProduto, codigoProduto)
+                        
+                    }
+                    createTable();
+                    
+                }
+                
+                let tbody = document.querySelector('.estoque-dados');
+                
+                let tr = document.createElement('tr');
+                tbody.appendChild(tr);
+
+                let item = document.createElement('td');
+                item.classList.add('item');
+                item.innerText = name;
+                
+                let quantity = document.createElement('td');
+                quantity.classList.add('quantity');
+                quantity.innerText = productQuantity;
+                
+                let type = document.createElement('td');
+                type.classList.add('tipo')
+                type.innerText = selectValue;
+                
+                let codigo = document.createElement('td');
+                codigo.classList.add('codigo');
+                codigo.innerText = code;
+                
+                tr.append(item, quantity, type, codigo)
+                
+                togglePopUpTable();
+                
+                document.getElementById('productName').value = '';
+                document.getElementById('productQuantity').value = '';
+                document.getElementById('type').value = 'Tipo';
+                select.style.color = 'gray'
+                document.getElementById('cod').value = '';
+                
+                nameInput.focus();
+                
+
+
+            }
         }
-
-
+        
     });
 
+//Escolher Tabela
+let seeStockTable = document.querySelector('.stock');
+let seeRetiredTable = document.querySelector('.retired');
+let stockTable = document.getElementById('estoque');
+let retiredTable = document.getElementById('retirada')
+
+seeStockTable.addEventListener('click', () => {
     
+    stockTable.style.display = 'block';
+    retiredTable.style.display = 'none';
+    seeRetiredTable.classList.remove('selected');
+    seeStockTable.classList.add('selected');
+
+    togglePopUpTable();
+    
+    
+})
+seeRetiredTable.addEventListener('click', () => {
+
+    retiredTable.style.display = 'block';
+    stockTable.style.display = 'none';
+    seeStockTable.classList.remove('selected');
+    seeRetiredTable.classList.add('selected');
+
+    togglePopUpTable();
+    
+})
+
+//Possui algum dado na tabela  
+function togglePopUpTable() {
+    let stockTableRows = document.querySelectorAll('#estoque tbody tr');
+    let retiredTableRows = document.querySelectorAll('#retirada tbody tr');
+    let popUpTable = document.getElementById("popUpTable");
+    let messageTable = document.querySelector('.popUpMessageTable');
+
+    // Verifica se ambas as tabelas estão vazias
+    if (stockTableRows.length === 0) {
+
+        popUpTable.style.display = 'block';
+        messageTable.textContent = 'Nenhum produto foi adicionado ao estoque!';
+
+    } else {
+
+        popUpTable.style.display = 'none';
+
+    }
+
+    if(retiredTableRows.length === 0 && stockTable.style.display === 'none') {
+
+        popUpTable.style.display = 'block';
+        messageTable.textContent = 'Nenhum produto foi retirado do estoque!';
+    }
+}
 
 
-// //Adiconar janela de aviso
-// document.getElementById("addProduct").addEventListener("click", function () {
-//     document.getElementById("popUp").style.display = "block";
-// });
 
-// document.getElementById("fecharPopUp").addEventListener("click", function () {
-//     document.getElementById("popUp").style.display = "none";
-// });
-
-// document.getElementById("ok").addEventListener("click", function () {
-//     document.getElementById("popUp").style.display = "none";
-// });
-
-// window.addEventListener("click", function (event) {
-//     if (event.target == document.getElementById("popUp")) {
-//         document.getElementById("popUp").style.display = "none";
-//     }
-// });
 
 //Texto ao clicar para baixar
 document.addEventListener("DOMContentLoaded", function () {
@@ -85,6 +318,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 1500); 
     }, 1000);
 
+    togglePopUpTable();
     });
 });
 
@@ -102,18 +336,4 @@ decreaseButton.addEventListener("click", () => {
 increaseButton.addEventListener("click", () => {
     const currentValue = parseInt(inputElement.value) || 0;
     inputElement.value = currentValue + 1;
-});
-
-
-//Mudar cor no select
-const selectElement = document.getElementById("type");
-
-selectElement.addEventListener("change", function () {
-    const selectedOption = selectElement.options[selectElement.selectedIndex];
-
-    if (selectedOption.classList.contains("white")) {
-        selectElement.style.color = "white";
-    } else {
-        selectElement.style.color = "gray"; 
-    }
 });
